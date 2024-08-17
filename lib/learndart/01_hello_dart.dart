c91886cd3cfb65bfeec7250ce61434ec289458ec
九级浪 <413612464@qq.com>
2024-08-18 07:10:40 +0800

    docs: dart-Hello Dart总结

--- lib/learndart/01_hello_dart.dart
+++ lib/learndart/01_hello_dart.dart
@@ -1,3 +1,10 @@
+/**
+ * 总结：
+ * 1. dart文件后缀为 .dart
+ * 2. dart文件中，main函数是程序的入口
+ * 3. dart文件中，main函数中，print函数可以输出内容到控制台
+ */
+
 void main(List<String> args) {
   print(args); // 控制台输入 dart 01_hello_dart.dart a b c
   print("Hello Dart");
