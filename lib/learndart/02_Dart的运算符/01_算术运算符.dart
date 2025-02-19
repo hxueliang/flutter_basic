e013f1d921a3b068963f52af18d2ffffc9c8b2f1
huangxl <413612464@qq.com>
2025-02-19 23:20:47 +0800

    feat: 算术运算符

---
+++ lib/learndart/02_Dart的运算符/01_算术运算符.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int a = 10;
+  int b = 3;
+
+  print(a + b); // 13
+  print(a - b); // 7
+  print(a * b); // 30
+  print(a / b); // 3.3333333333333335 浮点数
+  print(a ~/ b); // 3 取整
+  print(a % b); // 1 取余
+}
