0f20c2155debe14f0b06f8870025f8eb1f0cab8e
huangxl <413612464@qq.com>
2025-02-20 09:16:23 +0800

    feat: 关系运算符

---
+++ lib/learndart/02_Dart的运算符/02_关系运算符.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int a = 5;
+  int b = 3;
+
+  // ==  !=  >  <  >=  <=
+  print(a == b); // false
+  print(a != b); // true
+  print(a > b); // true
+  print(a < b); // false
+  print(a >= b); // true
+  print(a <= b); // false
+}
