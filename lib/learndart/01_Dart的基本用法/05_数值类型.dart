fb4ad5217a9be2bd77cae75b2c4cc2e4ecf8c76a
huangxl <413612464@qq.com>
2025-12-11 09:40:31 +0800

    feat: num类型

--- lib/learndart/01_Dart的基本用法/05_数值类型.dart
+++ lib/learndart/01_Dart的基本用法/05_数值类型.dart
@@ -1,12 +1,19 @@
 void main(List<String> args) {
-  // int类型
+  // int类型（整数类型）
   int a = 10;
   // a = 10.5; // 编译错误，int类型不能赋值为double类型
   print(a);
 
-  // double类型
+  // num类型（数值类型，建议变量可整可数小数时使用）
+  num d = 1;
+  print(d);
+  d = 2.5;
+  print(d);
+
+  // double类型（浮点类型，建议变量为小数使用）
   double b = 10.5;
-  // b = 10; // 正确，double类型可以赋值为int类型
+  print(b);
+  b = 10; // 正确，double类型可以赋值为int类型
   print(b);
 
   // 运算符 + - * / %
