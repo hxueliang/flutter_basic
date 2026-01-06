da6d7276a42ca318c092b34df3ba3800513a6fe6
huangxl <413612464@qq.com>
2026-01-06 12:43:50 +0800

    feat: Scaffold、AppBar、Container、Center、Text组件

--- lib/mainMaterialApp.dart
+++ lib/mainMaterialApp.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
       title: 'Flutter MaterialApp',
-      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
-      home: Scaffold()));
+      // theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('appBar'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('body'),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          height: 80,
+          child: Center(
+            child: Text('bottomNavigationBar'),
+          ),
+        ),
+      )));
 }
