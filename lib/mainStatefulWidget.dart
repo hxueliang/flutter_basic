19ff35ef23b68c277943f8e385522bf37ba9c832
huangxl <413612464@qq.com>
2026-01-06 15:24:18 +0800

    feat: 有状态组件生命周期

--- lib/mainStatefulWidget.dart
+++ lib/mainStatefulWidget.dart
@@ -5,17 +5,55 @@ void main(List<String> args) {
 }
 
 class MyPage extends StatefulWidget {
+  MyPage({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
+  _MyPageState createState() {
+    print("生命周期createState");
     return _MyPageState();
   }
 }
 
 class _MyPageState extends State<MyPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    print("生命周期initState");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    print("生命周期didChangeDependencies");
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant MyPage oldWidget) {
+    // TODO: implement didUpdateWidget
+    print("生命周期didUpdateWidget");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    print("生命周期deactivate");
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print("生命周期dispose");
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    print("生命周期build");
     return MaterialApp(
         title: '有状态组件',
         home: Scaffold(
