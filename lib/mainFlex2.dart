e1e294108efc6f4aa348b8ace7f0c9fe2fda4f13
huangxl <413612464@qq.com>
2026-01-06 23:42:24 +0800

    feat: Flex上中下，中占满剩余空间

--- lib/mainFlex2.dart
+++ lib/mainFlex2.dart
@@ -12,28 +12,25 @@ class MainPage extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text("Flox 组件"),
+              title: Text("Flox 组件 2"),
             ),
             body: Container(
                 width: double.infinity,
                 height: double.infinity,
                 color: Colors.amber,
-                child: Flex(direction: Axis.horizontal, children: [
+                child: Flex(direction: Axis.vertical, children: [
+                  Container(
+                    color: Colors.green,
+                    height: 80,
+                  ),
                   Expanded(
-                      flex: 2,
                       child: Container(
-                        color: Colors.red,
-                        width: 100,
-                        height: 100,
-                      )),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.green,
-                      width: 100,
-                      height: 100,
-                    ),
-                  )
+                    color: Colors.red,
+                  )),
+                  Container(
+                    color: Colors.blue,
+                    height: 80,
+                  ),
                 ]))));
   }
 }
