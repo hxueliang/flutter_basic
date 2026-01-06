de9fe21d75fb4f7ec21a6909d08ff3d1a98ce383
huangxl <413612464@qq.com>
2026-01-07 05:04:52 +0800

    feat: Stack组件

---
+++ lib/mainStack.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Wrap 组件"),
+            ),
+            body: Container(
+                color: Colors.amber,
+                width: double.infinity,
+                height: double.infinity,
+                child: Stack(alignment: Alignment.center, children: [
+                  Container(
+                    color: Colors.red,
+                    width: 300,
+                    height: 300,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 200,
+                    height: 200,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    width: 20,
+                    height: 20,
+                  ),
+                ]))));
+  }
+}
