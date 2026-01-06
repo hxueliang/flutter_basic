33bcb43c451c2828b3aace67fb812913af4df00d
huangxl <413612464@qq.com>
2026-01-06 11:03:17 +0800

    feat: MaterialApp

---
+++ lib/mainMaterialApp.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+      title: 'Flutter MaterialApp',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+      home: Scaffold()));
+}
