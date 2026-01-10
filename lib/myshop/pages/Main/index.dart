6ec8d96a37bfae45b28f3ce6aba88ebc8e874bc5
huangxl <413612464@qq.com>
2026-01-10 15:42:35 +0800

    feat: MyShop-实现底部TabBar

--- lib/myshop/pages/Main/index.dart
+++ lib/myshop/pages/Main/index.dart
@@ -8,17 +8,64 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _items = [
+    {
+      'icon': 'lib/myshop/assets/ic_home.png',
+      'activeIcon': 'lib/myshop/assets/ic_home_fill.png',
+      'label': '首页'
+    },
+    {
+      'icon': 'lib/myshop/assets/ic_message.png',
+      'activeIcon': 'lib/myshop/assets/ic_message_fill.png',
+      'label': '消息'
+    },
+    {
+      'icon': 'lib/myshop/assets/ic_cart.png',
+      'activeIcon': 'lib/myshop/assets/ic_cart_fill.png',
+      'label': '购物车'
+    },
+    {
+      'icon': 'lib/myshop/assets/ic_my.png',
+      'activeIcon': 'lib/myshop/assets/ic_my_fill.png',
+      'label': '我的'
+    },
+  ];
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Main Page'),
+        body: Container(
+          child: Center(
+            child: Text('Main Page'),
+          ),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+          onTap: (index) {
+            _currentIndex = index;
+            setState(() {});
+          },
+          currentIndex: _currentIndex,
+          items: _getBottomNavigationBar(),
+        ));
+  }
+
+  List<BottomNavigationBarItem> _getBottomNavigationBar() {
+    return List.generate(_items.length, (index) {
+      return BottomNavigationBarItem(
+          icon: Image.asset(
+            _items[index]['icon']!,
+            width: 30,
+            height: 30,
+          ),
+          activeIcon: Image.asset(
+            _items[index]['activeIcon']!,
+            width: 30,
+            height: 30,
+          ),
+          label: _items[index]['label']);
+    });
   }
 }
